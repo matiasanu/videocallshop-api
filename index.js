965a3d51dd0f7d11a50861aa6390339ead1f8bfd
Gary <alemmartin@gmail.com>
2019-04-29 12:44:28 -0300

    29-04-2019 - Init empty express project

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
