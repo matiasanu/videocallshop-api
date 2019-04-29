da82682b42f710d1ceb5e9b5ae6c5de0f3a389ee
Gary <alemmartin@gmail.com>
2019-04-29 12:57:28 -0300

    29-04-2019 - Test autodeploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Laburá Mauro!'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
