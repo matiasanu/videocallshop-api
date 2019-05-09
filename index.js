0514b9ecc82e008f54a10b21267fd98f6e52f8a8
Gary <alemmartin@gmail.com>
2019-05-09 16:42:53 -0300

    09-05-2019 - WIP

--- index.js
+++ index.js
@@ -3,7 +3,31 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Laburá Mauro!'));
+const { Client } = require('pg');
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
+try {
+    client.connect();
+} catch (e) {
+    console.log(e);
+}
+
+app.get('/', (req, res) => {
+    let results = client.query('select * from pruebita;', (err, res) => {
+        if (err) throw err;
+        for (let row of res.rows) {
+            console.log(JSON.stringify(row));
+        }
+        client.end();
+    });
+
+    console.log(results);
+
+    res.send(results);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
