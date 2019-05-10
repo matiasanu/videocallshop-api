ef3fafb9ce40d854c2efe3c9db2ecbd016fa48d8
Gary Martin <alemmartin@gmail.com>
2019-05-10 14:17:30 -0300

    10-05-2019 - Implement pg connection test

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { Client } = require('pg');
 
 config = {
     ssl: process.env.PGSSL ? process.env.PGSSL : true,
+    connectionString: process.env.DATABASE_URL,
 };
 
 const client = new Client(config);
