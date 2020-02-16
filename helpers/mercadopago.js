950e522a44fb93fe3a1741dddb65e437ca603ece
Gary Martin <alemmartin@gmail.com>
2020-02-16 20:37:46 -0300

    Rollback old way

--- helpers/mercadopago.js
+++ helpers/mercadopago.js
@@ -4,6 +4,24 @@ const fetch = require('node-fetch');
 const createPreference = (accessToken, items, externalReference) => {
     mp.configurations.setAccessToken(accessToken);
 
+    let mpOptions = {};
+    // check if mercadopago is in productive or sandbox mode
+    if (process.env.MERCADOPAGO_SANDBOX === 'TRUE') {
+        //sandbox
+        mpOptions.sandbox = true;
+        mpOptions.access_token =
+            process.env.MERCADOPAGO_MARKETPLACE_SANDBOX_ACCESS_TOKEN;
+        console.log('Generating MERCADOPAGO SANDBOX link', mpOptions);
+    } else {
+        //productive
+        mpOptions.client_id = process.env.MERCADOPAGO_MARKETPLACE_CLIENT_ID;
+        mpOptions.client_secret =
+            process.env.MERCADOPAGO_MARKETPLACE_CLIENT_SECRET;
+        console.log('Generating MERCADOPAGO PRODUCTIVE link', mpOptions);
+    }
+
+    mp.configure(mpOptions);
+
     //const accessToken = generateToken();
     const preferenceOptions = {
         external_reference: externalReference,
