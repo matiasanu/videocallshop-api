a82644d68145418ca062a9d8f0cb3e3fa1b2b6ca
Gary Martin <alemmartin@gmail.com>
2020-02-16 11:50:44 -0300

    Hardcode https protocol on mercadopago

--- helpers/mercadopago.js
+++ helpers/mercadopago.js
@@ -36,10 +36,7 @@ const createPreference = (
 };
 
 const getAuthorizationUrl = (store, protocol, host) => {
-    console.log(protocol);
-    console.log(protocol);
-    console.log(protocol);
-    return `https://auth.mercadopago.com.ar/authorization?client_id=${store.mercadopagoClientId}&APP_ID=${process.env.MERCADOPAGO_MARKETPLACE_APP_ID}&response_type=code&platform_id=mp&redirect_uri=${protocol}://${host}/mercadopago/store-authorization-code?storeId=${store.storeId}`;
+    return `https://auth.mercadopago.com.ar/authorization?client_id=${store.mercadopagoClientId}&APP_ID=${process.env.MERCADOPAGO_MARKETPLACE_APP_ID}&response_type=code&platform_id=mp&redirect_uri=https://${host}/mercadopago/store-authorization-code?storeId=${store.storeId}`;
 };
 
 module.exports = {
