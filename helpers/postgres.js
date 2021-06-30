ec28decabc0696f45aeea0e70b9d9f8755657300
Gary Martin <alemmartin@gmail.com>
2021-06-30 13:36:18 -0300

    Add ssl rejectUnauthorized: false

--- helpers/postgres.js
+++ helpers/postgres.js
@@ -3,7 +3,10 @@ var pgCamelCase = require('pg-camelcase');
 var revertCamelCase = pgCamelCase.inject(pg);
 
 config = {
-    ssl: process.env.PGSSL ? process.env.PGSSL : true,
+    // ssl: process.env.PGSSL ? process.env.PGSSL : true,
+    ssl: {
+        rejectUnauthorized: false
+    },
     connectionString: process.env.DATABASE_URL,
 };
 const pool = new pg.Pool(config);
